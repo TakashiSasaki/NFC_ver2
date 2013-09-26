d5c8b0bec8f79d8ea8cbb4c795b92a69069afa89
Takashi SASAKI <takashi316@gmail.com>
2013-09-26 18:27:27 +0900

    UserDatabaseHelper.java was moved to db package

--- src/com/gmail/matsushige/nfcv2/db/UsersDatabase.java
+++ src/com/gmail/matsushige/nfcv2/db/UsersDatabase.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.gmail.matsushige.nfcv2.Nfc_simple;
-import com.gmail.matsushige.nfcv2.UsersDatabaseHelper;
 
 public class UsersDatabase {
 	public static String usersText = "";
