2d170ba07c3003546d2660001221f3c1d54f81fa
Takashi SASAKI <takashi316@gmail.com>
2013-09-28 16:09:19 +0900

    UnknownUserActivity.java was added

---
+++ src/com/gmail/matsushige/nfcv2/UnknownUserActivity.java
@@ -0,0 +1,25 @@
+package com.gmail.matsushige.nfcv2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.gmail.matsushige.R;
+
+public class UnknownUserActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.unknown_user_activity);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.unknown_user, menu);
+        return true;
+    }
+    
+}
