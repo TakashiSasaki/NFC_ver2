a45518842b7f56ee3be4f2ba015c3909b72f8968
Takashi SASAKI <takashi316@gmail.com>
2013-09-29 21:42:58 +0900

    assertions were added to Preference.java

--- src/com/gmail/matsushige/nfcv2/util/Preference.java
+++ src/com/gmail/matsushige/nfcv2/util/Preference.java
@@ -30,12 +30,16 @@ public class Preference {
     }//putBoolean
 
     public String getUserType() {
-        return this.sharedPreferences.getString("userType", null);
-    }
+        String user_type = this.sharedPreferences.getString("userType", null);
+        if (user_type != null) assert (!user_type.equals(""));
+        return user_type;
+    }//getUserType
 
     public String getUserId() {
-        return this.sharedPreferences.getString("userId", null);
-    }
+        String user_id = this.sharedPreferences.getString("userId", null);
+        if (user_id != null) assert (!user_id.equals(""));
+        return user_id;
+    }//getUserId
 
     public String getOutletId() {
         return this.sharedPreferences.getString("outletId", "AA");
@@ -48,15 +52,16 @@ public class Preference {
     }
 
     public void setTypeAndId(String type, String id) {
-        if("".equals(type)) throw new IllegalArgumentException("card type should not be empty string");
-        if("".equals(id)) throw new IllegalArgumentException("card id should not be empty string");
+        if ("".equals(type))
+            throw new IllegalArgumentException("card type should not be empty string");
+        if ("".equals(id)) throw new IllegalArgumentException("card id should not be empty string");
         SharedPreferences.Editor edit = this.sharedPreferences.edit();
         edit.putString("userType", type);
         edit.putString("userId", id);
         edit.commit();
     }//setTypeAndId
 
-    public void resetPreference(){
+    public void resetPreference() {
         SharedPreferences.Editor edit = this.sharedPreferences.edit();
         edit.putString("userType", null);
         edit.putString("userId", null);
