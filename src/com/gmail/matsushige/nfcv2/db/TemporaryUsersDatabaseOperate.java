1b4403aae720e91ada295d89c45677c33229d7ab
Takashi SASAKI <takashi316@gmail.com>
2013-09-29 19:05:44 +0900

    reformatting

--- src/com/gmail/matsushige/nfcv2/db/TemporaryUsersDatabaseOperate.java
+++ src/com/gmail/matsushige/nfcv2/db/TemporaryUsersDatabaseOperate.java
@@ -13,13 +13,14 @@ import java.util.Date;
 
 public class TemporaryUsersDatabaseOperate {
 
-	private final static String TAG = "TemporaryUsersDatabaseOperate";
-	public static String tempText = "";
+    private final static String TAG = "TemporaryUsersDatabaseOperate";
+    public static String tempText = "";
     private SQLiteOpenHelper temporaryUserDatabaseHelper;
 
     private static TemporaryUsersDatabaseOperate theInstance;
-    synchronized public static TemporaryUsersDatabaseOperate getTheInstance(Context context){
-        if(theInstance != null){
+
+    synchronized public static TemporaryUsersDatabaseOperate getTheInstance(Context context) {
+        if (theInstance != null) {
             return theInstance;
         }
         theInstance = new TemporaryUsersDatabaseOperate(context);
@@ -28,86 +29,87 @@ public class TemporaryUsersDatabaseOperate {
 
 
     private Context context;
-    private TemporaryUsersDatabaseOperate(Context context){
+
+    private TemporaryUsersDatabaseOperate(Context context) {
         this.temporaryUserDatabaseHelper = new TemporaryUsersDatabaseHelper(context);
     }//TemporaryUsersDatabaseOperate (private constructor)
 
-	public void write(String type, String id, String regCode){
-		long regTime = Calendar.getInstance().getTimeInMillis();
-		SQLiteDatabase db = this.temporaryUserDatabaseHelper.getWritableDatabase();
-		ContentValues cv = new ContentValues();
-		cv.put("card_type", type);
-		cv.put("card_id", id);
-		cv.put("register_code", regCode);
-		cv.put("register_time", regTime);
-		cv.put("send_check", false);
-		db.insert("tempusers", null, cv);
-		db.close();
-	}
-	
-	public void read(){
-		tempText = "";
-		SQLiteDatabase db = this.temporaryUserDatabaseHelper
-				.getReadableDatabase();
-		Cursor c = db.query("tempusers", null, null, null, null, null, null);
-		c.moveToFirst();
-		for (int i = 0; i < c.getCount(); ++i) {
-			long serial = c.getLong(c.getColumnIndex("serial"));
-			String type = c.getString(c.getColumnIndex("card_type"));
-			String id = c.getString(c.getColumnIndex("card_id"));
-			String register_code = c.getString(c.getColumnIndex("register_code"));
-			long register_time = c.getLong(c.getColumnIndex("register_time"));
-			String regTime = (new Date(register_time)).toLocaleString();
-			String send_check = c.getString(c.getColumnIndex("send_check"));
-			
-			tempText += serial + " " + type + " " + id + " " + register_code + " " + regTime + " " + send_check + "\n";
-			c.moveToNext();
-		}// for
-		db.close();
-	}
-	
-	public void deleteOldData(){
-		long currentTime = Calendar.getInstance().getTimeInMillis();
-		SQLiteDatabase db = temporaryUserDatabaseHelper.getReadableDatabase();
-		String[] key = {"1"}; // send_checkが"1"であるか 
-		Cursor cursor = db.query("tempusers", null, "send_check = ?", key, null, null, null);
-		cursor.moveToFirst();
-		for(int i = 0; i < cursor.getCount(); i++){
-			long serial = cursor.getLong(cursor.getColumnIndex("serial"));
-			long register_time = cursor.getLong(cursor.getColumnIndex("register_time"));
-			long difference = currentTime - register_time;
-			if(difference > AlarmManager.INTERVAL_FIFTEEN_MINUTES){
-				String[] target = {"" + serial}; // 該当する通し番号を対象に指定
-				db.delete("tempusers", "serial = ?", target);
-				Log.d(TAG, "DELETE");
-			}else{
-				Log.d(TAG, "NOT_DELETE");
-			}// else
-			cursor.moveToNext();
-		}// for
-		cursor.close();
-		db.close();
-	}// deleteOldData
-	
-	public TemporaryUser getRegisteredData(String type, String id){
+    public void write(String type, String id, String regCode) {
+        long regTime = Calendar.getInstance().getTimeInMillis();
+        SQLiteDatabase db = this.temporaryUserDatabaseHelper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("card_type", type);
+        cv.put("card_id", id);
+        cv.put("register_code", regCode);
+        cv.put("register_time", regTime);
+        cv.put("send_check", false);
+        db.insert("tempusers", null, cv);
+        db.close();
+    }
+
+    public void read() {
+        tempText = "";
+        SQLiteDatabase db = this.temporaryUserDatabaseHelper
+                .getReadableDatabase();
+        Cursor c = db.query("tempusers", null, null, null, null, null, null);
+        c.moveToFirst();
+        for (int i = 0; i < c.getCount(); ++i) {
+            long serial = c.getLong(c.getColumnIndex("serial"));
+            String type = c.getString(c.getColumnIndex("card_type"));
+            String id = c.getString(c.getColumnIndex("card_id"));
+            String register_code = c.getString(c.getColumnIndex("register_code"));
+            long register_time = c.getLong(c.getColumnIndex("register_time"));
+            String regTime = (new Date(register_time)).toLocaleString();
+            String send_check = c.getString(c.getColumnIndex("send_check"));
+
+            tempText += serial + " " + type + " " + id + " " + register_code + " " + regTime + " " + send_check + "\n";
+            c.moveToNext();
+        }// for
+        db.close();
+    }
+
+    public void deleteOldData() {
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        SQLiteDatabase db = temporaryUserDatabaseHelper.getReadableDatabase();
+        String[] key = {"1"}; // send_checkが"1"であるか
+        Cursor cursor = db.query("tempusers", null, "send_check = ?", key, null, null, null);
+        cursor.moveToFirst();
+        for (int i = 0; i < cursor.getCount(); i++) {
+            long serial = cursor.getLong(cursor.getColumnIndex("serial"));
+            long register_time = cursor.getLong(cursor.getColumnIndex("register_time"));
+            long difference = currentTime - register_time;
+            if (difference > AlarmManager.INTERVAL_FIFTEEN_MINUTES) {
+                String[] target = {"" + serial}; // 該当する通し番号を対象に指定
+                db.delete("tempusers", "serial = ?", target);
+                Log.d(TAG, "DELETE");
+            } else {
+                Log.d(TAG, "NOT_DELETE");
+            }// else
+            cursor.moveToNext();
+        }// for
+        cursor.close();
+        db.close();
+    }// deleteOldData
+
+    public TemporaryUser getRegisteredData(String type, String id) {
         TemporaryUser temporary_user = new TemporaryUser(type, id);
-		SQLiteDatabase db = this.temporaryUserDatabaseHelper.getReadableDatabase();
-		String where = "card_type = ?";
-		String[] where_arg = {type};
-		Cursor cursor = db.query("tempusers", null, where, where_arg, null, null, null);
-		while(cursor.moveToNext()){
-			String registeredId = cursor.getString(cursor.getColumnIndex("card_id"));
-			if(id.equals(registeredId)){
-				String serial = cursor.getString(cursor.getColumnIndex("serial"));
-				String regcode = cursor.getString(cursor.getColumnIndex("register_code"));
+        SQLiteDatabase db = this.temporaryUserDatabaseHelper.getReadableDatabase();
+        String where = "card_type = ?";
+        String[] where_arg = {type};
+        Cursor cursor = db.query("tempusers", null, where, where_arg, null, null, null);
+        while (cursor.moveToNext()) {
+            String registeredId = cursor.getString(cursor.getColumnIndex("card_id"));
+            if (id.equals(registeredId)) {
+                String serial = cursor.getString(cursor.getColumnIndex("serial"));
+                String regcode = cursor.getString(cursor.getColumnIndex("register_code"));
                 temporary_user.setOwnerName("ゲスト" + serial);
                 temporary_user.setRegistrationCode(regcode);
                 return temporary_user;
-			}// if
-		}// while
-		cursor.close();
-		db.close();
+            }// if
+        }// while
+        cursor.close();
+        db.close();
         return temporary_user;
-	}// checkRegisteredData
+    }// checkRegisteredData
 
 }
