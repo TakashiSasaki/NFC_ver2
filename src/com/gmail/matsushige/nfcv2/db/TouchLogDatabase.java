4a62901ad3bd9eb64ccd407d2192e961cab4611d
Takashi SASAKI <takashi316@gmail.com>
2013-09-30 02:45:35 +0900

    reformatting

--- src/com/gmail/matsushige/nfcv2/db/TouchLogDatabase.java
+++ src/com/gmail/matsushige/nfcv2/db/TouchLogDatabase.java
@@ -6,31 +6,31 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 public class TouchLogDatabase {
-	public static String logText = "";
+    public static String logText = "";
 
-	public static void write(Context context,String type, String id, long timestamp){
-		ContentValues cv = new ContentValues();
-		cv.put("type", type );
-		cv.put("ID", id);
-		cv.put("timestamp", timestamp);
-		SQLiteDatabase db = (new TouchLogDatabaseHelper(context)).getReadableDatabase();
-		db.insert("touch", null, cv);
-		db.close();
-	}// write
+    public static void write(Context context, String type, String id, long timestamp) {
+        ContentValues cv = new ContentValues();
+        cv.put("type", type);
+        cv.put("ID", id);
+        cv.put("timestamp", timestamp);
+        SQLiteDatabase db = (new TouchLogDatabaseHelper(context)).getReadableDatabase();
+        db.insert("touch", null, cv);
+        db.close();
+    }// write
 
-	public static void read(Context context){
-		logText = "";
-		SQLiteDatabase db = (new TouchLogDatabaseHelper(context)).getReadableDatabase();
-		Cursor c = db.query("touch", null, null, null, null, null, null);
-		c.moveToFirst();
-		for (int i = 0; i < c.getCount(); ++i) {
-			long _id = c.getLong(c.getColumnIndex("_id"));
-			String type = c.getString(c.getColumnIndex("type"));
-			String id = c.getString(c.getColumnIndex("ID"));
-			long timestamp = c.getLong(c.getColumnIndex("timestamp"));
-			logText += _id + " " + type + " " + id + " " + timestamp + "\n";
-			c.moveToNext();
-		}// for
-		db.close();
-	}// read
+    public static void read(Context context) {
+        logText = "";
+        SQLiteDatabase db = (new TouchLogDatabaseHelper(context)).getReadableDatabase();
+        Cursor c = db.query("touch", null, null, null, null, null, null);
+        c.moveToFirst();
+        for (int i = 0; i < c.getCount(); ++i) {
+            long _id = c.getLong(c.getColumnIndex("_id"));
+            String type = c.getString(c.getColumnIndex("type"));
+            String id = c.getString(c.getColumnIndex("ID"));
+            long timestamp = c.getLong(c.getColumnIndex("timestamp"));
+            logText += _id + " " + type + " " + id + " " + timestamp + "\n";
+            c.moveToNext();
+        }// for
+        db.close();
+    }// read
 }// TouchLogDatabase
--- src/com/gmail/matsushige/nfcv2/db/TouchLogDatabaseHelper.java
+++ src/com/gmail/matsushige/nfcv2/db/TouchLogDatabaseHelper.java
@@ -8,22 +8,22 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class TouchLogDatabaseHelper extends SQLiteOpenHelper {
 
-	public TouchLogDatabaseHelper(Context context) {
-		super(context,"NFC.log.sqlite", null, 1);
-		// TODO Auto-generated constructor stub
-	}
+    public TouchLogDatabaseHelper(Context context) {
+        super(context, "NFC.log.sqlite", null, 1);
+        // TODO Auto-generated constructor stub
+    }
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
-		db.execSQL("CREATE TABLE touch("
-				+"_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-				+"type TEXT, ID TEXT, timestamp INTEGER);");
-	}
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL("CREATE TABLE touch("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "type TEXT, ID TEXT, timestamp INTEGER);");
+    }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 }
