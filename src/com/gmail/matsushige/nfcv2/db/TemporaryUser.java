2f6cb2b7efb361cc4d9643de6977ab933e89a439
Takashi SASAKI <takashi316@gmail.com>
2013-09-29 20:30:40 +0900

    getCardId and getCardType was added as accessors.

--- src/com/gmail/matsushige/nfcv2/db/TemporaryUser.java
+++ src/com/gmail/matsushige/nfcv2/db/TemporaryUser.java
@@ -9,23 +9,32 @@ public class TemporaryUser {
     String ownerName;
     String registrationCode;
 
-    public TemporaryUser(String card_type, String card_id){
+    public TemporaryUser(String card_type, String card_id) {
         this.cardId = card_id;
         this.cardType = card_type;
     }
 
-    public void setOwnerName(String owner_name){
+    public String getCardType() {
+        return this.cardType;
+    }
+
+    public String getCardId() {
+        return this.cardId;
+    }
+
+    public void setOwnerName(String owner_name) {
         this.ownerName = owner_name;
     }
 
-    public void setRegistrationCode(String registration_code){
+    public void setRegistrationCode(String registration_code) {
         this.registrationCode = registration_code;
     }
-    public String getOwnerName(){
+
+    public String getOwnerName() {
         return this.ownerName;
     }
 
-    public String getRegistrationCode(){
+    public String getRegistrationCode() {
         return this.registrationCode;
     }
 }//TemporaryUser
