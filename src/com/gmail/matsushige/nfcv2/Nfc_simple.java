f6c82f95744f5ad0be32f6691d07faf4b90bdcf5
Takashi SASAKI <takashi316@gmail.com>
2013-10-01 01:48:50 +0900

    version 1.3.3 starts here

--- src/com/gmail/matsushige/nfcv2/Nfc_simple.java
+++ src/com/gmail/matsushige/nfcv2/Nfc_simple.java
@@ -21,7 +21,6 @@ import com.gmail.matsushige.nfcv2.db.TouchLogDatabase;
 import java.util.Calendar;
 
 public class Nfc_simple extends TimerActivity {
-    //private TextView timeText;
     private byte[] id;
     private String type = "";
 
@@ -29,7 +28,7 @@ public class Nfc_simple extends TimerActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nfc_main_start);
-        setTitle("みんなでおでんき ソーシャル・コンセント 待受中");
+        setTitle("みんなでおでんき ソーシャル・コンセント カード待受中");
 
         Relay.test(getApplicationContext());
     }// onCreate
