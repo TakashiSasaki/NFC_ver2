e9f58ead28754a2ef39acef5e49efb9438951023
Takashi SASAKI <takashi316@gmail.com>
2013-10-01 05:36:31 +0900

    UI improved in RegularUserActivity

--- src/com/gmail/matsushige/nfcv2/activity/RegularUserActivity.java
+++ src/com/gmail/matsushige/nfcv2/activity/RegularUserActivity.java
@@ -4,35 +4,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
 import com.gmail.matsushige.R;
-import com.gmail.matsushige.nfcv2.RelayOpenTimerIntentService;
 import com.gmail.matsushige.nfcv2.CountTimeAllUser;
 import com.gmail.matsushige.nfcv2.Nfc_simple;
 import com.gmail.matsushige.nfcv2.Relay;
+import com.gmail.matsushige.nfcv2.RelayOpenTimerIntentService;
 import com.gmail.matsushige.nfcv2.db.ActLogDatabase;
 
 import java.util.Calendar;
 
 public class RegularUserActivity extends TimerActivity {
+    static final int MAX_RELAY_CLOSE_SECONDS = 10;
+    private EditText editTextUserName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nfc_main_regular);
         setTitle("みんなでおでんき ソーシャル・コンセント 登録使用者");
+        editTextUserName = (EditText) findViewById(R.id.editTextUserName);
     }//onCreate
 
     @Override
     protected void onResume() {
         super.onResume();
+        Relay.setRelayOnResume();
         reguUsersPic();
+        CountTimeAllUser.startCountTimeAllUser(this);
     }//onResume
 
     private void reguUsersPic() {
+        editTextUserName.setText(preference.getCardOwner());
         TextView userNameText = (TextView) findViewById(R.id.textViewUserName);
-        userNameText.setText(preference.getCardOwner() + "さん、こんにちは。");
 
         Button powerCancelButton = (Button) findViewById(R.id.buttonPowerCancel);
         ToggleButton relay1Toggle = (ToggleButton) findViewById(R.id.toggleRelay1);
@@ -68,13 +75,9 @@ public class RegularUserActivity extends TimerActivity {
         relay1Toggle.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                if (CountTimeAllUser.isUsed) {
-                    //CountTimeAllUser.retainUserData = true;
-                    CountTimeAllUser.stop();
-                }
+                stopCloseTimer();
                 if (!(RelayOpenTimerIntentService.isUsed)) {
-                    RelayOpenTimerIntentService.startCountRelayTime(getApplicationContext(), 60 * 60);
+                    RelayOpenTimerIntentService.startCountRelayTime(getApplicationContext(), MAX_RELAY_CLOSE_SECONDS);
                 }
                 if (Relay.getRelay(0).isOpened()) {
                     Relay.getRelay(0).close();
@@ -89,13 +92,9 @@ public class RegularUserActivity extends TimerActivity {
         relay2Toggle.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                if (CountTimeAllUser.isUsed) {
-                    //CountTimeAllUser.retainUserData = true;
-                    CountTimeAllUser.stop();
-                }
+                stopCloseTimer();
                 if (!(RelayOpenTimerIntentService.isUsed)) {
-                    RelayOpenTimerIntentService.startCountRelayTime(getApplicationContext(), 60 * 60);
+                    RelayOpenTimerIntentService.startCountRelayTime(getApplicationContext(), MAX_RELAY_CLOSE_SECONDS);
                 }
                 if (Relay.getRelay(1).isOpened()) {
                     Relay.getRelay(1).close();
