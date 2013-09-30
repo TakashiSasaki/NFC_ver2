9bcc7e38acbe8244b9cf2cb3a93c34979d048105
Takashi SASAKI <takashi316@gmail.com>
2013-09-30 17:54:19 +0900

    fix: crushes when broadcast intent is received after the activity disappears

--- src/com/gmail/matsushige/nfcv2/activity/TimerActivity.java
+++ src/com/gmail/matsushige/nfcv2/activity/TimerActivity.java
@@ -14,6 +14,7 @@ import com.gmail.matsushige.nfcv2.CountTimeAllUser;
 import com.gmail.matsushige.nfcv2.Nfc_simple;
 
 public class TimerActivity extends BaseActivity {
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -41,12 +42,13 @@ public class TimerActivity extends BaseActivity {
             Bundle bundle = intent.getExtras();
             int count = bundle.getInt("count");
             /** 本登録ユーザ画面であればテキスト表示 */
-            ((TextView) findViewById(R.id.textViewRelayCountdown)).setText("使用中です。\nあと" + secToMin(CountRelayTime.getMaxCount() - count) + "で通電を終了します");
+            TextView textViewRelayCountdown = (TextView) findViewById(R.id.textViewRelayCountdown);
+            if (textViewRelayCountdown != null)
+                textViewRelayCountdown.setText("あと" + secToMin(CountRelayTime.getMaxCount() - count) + "で通電を終了します");
             if (count >= CountRelayTime.getMaxCount()) {
                 preference.resetPreference();
-                ((TextView) findViewById(R.id.textViewRelayCountdown)).setText("使用可能です。");
+                if (textViewRelayCountdown != null) textViewRelayCountdown.setText("使用可能です。");
                 Toast.makeText(getApplicationContext(), "使用可能時間が過ぎました", Toast.LENGTH_SHORT).show();
-                //changeMainXto0();
             }// if
         }// onReceive
 
@@ -84,4 +86,3 @@ public class TimerActivity extends BaseActivity {
     };// closeCountdownBroadcastReceiver
 
 }//TimerActivity
-
